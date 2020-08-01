81d4f27c5d2c02e579e0ff28d4a735afcd67658f
08darknight1 <39628779+08darknight1@users.noreply.github.com>
2020-07-31 22:13:26 -0300

    Revert "LOASODASODA"
    This reverts commit 7548291f0f3a4d0cdfca56efad7cbfba667e3dd7.

---
+++ Assets/Nathan/N_Scripts/MouseCollider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCollider : MonoBehaviour
+{
+    void OnMouseOver()
+    {
+        Debug.Log("PASSOU EM "+gameObject.name);
+    }
+}
