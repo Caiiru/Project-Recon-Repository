82fe175f035eca686169822cc4b95cec3cb26de9
Caiiru <caiogome125@gmail.com>
2020-08-11 21:10:30 -0300

    turnHud arrumado e action command1.0 implementado

--- Assets/Scripts/battleSystem.cs
+++ Assets/Scripts/battleSystem.cs
@@ -11,11 +11,12 @@ public class battleSystem : MonoBehaviour
     public List<GameObject> chars = new List<GameObject>();
     public BattleState state;
 
-     //--------- BOTOES ----------
-    private bool endTurn = false; //Voltar para a lista e setar o prox turno
+    //--------- BOTOES ----------
+    [SerializeField] private bool endTurn = false; //Voltar para a lista e setar o prox turno
     //--------- PLAYER ----------
     private bool playerHasPlayed = false;
     private bool setedPlayerTurn = false;
+    private bool suceffulAttack;
     //--------- PLAYER ----------
     //--------- ENEMY -----------
     private bool enemyHasPlayed = false;
@@ -35,46 +36,71 @@ public class battleSystem : MonoBehaviour
     //private bool comp2Action = false;
     public GameObject skullHud;
     public GameObject zeroHud;
+    public GameObject Comp1Hud;
+    public GameObject Comp2Hud;
     public Transform pos1;
     public Transform pos2;
+    public Transform pos3;
+    public Transform pos4;
     public GameObject commandsCanvas;
+    public c_action acctionC;
+
 
 
     void Start()
     {
+        acctionC = acctionC.GetComponent<c_action>();
         state = BattleState.START;
         enemyHUD.setHUD(enemyPrefab.GetComponent<Unit>());
         playerHud.setHUD(playerPrefab.GetComponent<Unit>());
         battleStatusText.text = "Starting Battle";
     }
 
+
     private void Update()
     {
-        if(endTurn)
+        if (acctionC.Retornar())
         {
-            endTurn= false;
+            acctionC.Resetar();
+            
+            suceffulAttack = true;
+
+        }
+        else if (acctionC.Retornar() == false & acctionC.RetornarErro() == true)
+        {
+            acctionC.Resetar();
+            
+            suceffulAttack = false;
+        }
+
+        if (endTurn)
+        {
+            endTurn = false;
+            
+            enemyPrefab.GetComponent<TimerForTurn>().Reiniciar();
+            playerPrefab.GetComponent<TimerForTurn>().Reiniciar();
             state = BattleState.SETTURNS;
         }
-        
+
         switch (state.ToString())
         {
             case "START":
                 CreateLisT();
-                 break;
+                break;
             case "PLAYERTURN":
-                    if (setedPlayerTurn == false)
-                    { _playerTurn(); }
-                    break;
+                if (setedPlayerTurn == false)
+                { _playerTurn(); }
+                break;
             case "ENEMYTURN":
-                    if(setedEnemyTurn == false)
-                    { _enemyTurn(); }
-                    break;
+                if (setedEnemyTurn == false)
+                { _enemyTurn(); }
+                break;
             case "EOR":
-                    EndOfRound();
-                    break;
+                EndOfRound();
+                break;
             case "SETTURNS":
-                    SetTurns();
-                    break;
+                SetTurns();
+                break;
         }
     }
 
@@ -85,9 +111,25 @@ public class battleSystem : MonoBehaviour
         activateCommandsMenu();
         battleStatusText.text = "Your Turn";
     }
+    IEnumerator checkAttack(GameObject enemyAttacked)
+    {
+        yield return new WaitForSeconds(3f);
+        playerHasPlayed = true;
+        if (suceffulAttack)
+        {
+            StartCoroutine(PlayerAttack(enemyAttacked));
+
+        }
+        else if (suceffulAttack == false)
+        {
+            endTurn = true;
+        }
+    }
+
 
     IEnumerator PlayerAttack(GameObject enemyAttacked)
     {
+        yield return new WaitForSeconds(1f);
         playerPrefab.GetComponent<Unit>().playSound(1);
         
         bool isDead = false;
@@ -125,18 +167,20 @@ public class battleSystem : MonoBehaviour
         }
         else
         {
+            Debug.Log("EndTurn");
             endTurn = true;
         }
     }
     void _enemyTurn()
     {
+        battleStatusText.text = "Enemy Turn";
         enemyPrefab.GetComponent<TimerForTurn>().Iniciar(2);
 
         if (enemyPrefab.GetComponent<TimerForTurn>().Sinalizar())
         {
             enemyHasPlayed = true;
             setedEnemyTurn = true;
-            battleStatusText.text = "Enemy Turn";
+            
             Debug.Log("Enemy turno");
 
             enemyPrefab.GetComponent<Unit>().playSound(1);
@@ -166,8 +210,8 @@ public class battleSystem : MonoBehaviour
         }
         else
         {
-            StartCoroutine(PlayerAttack(enemyAttacked));
-            playerHasPlayed = true;
+            acctionC.Ativar();
+            StartCoroutine(checkAttack(enemyAttacked));
         }
     }
 
@@ -198,7 +242,7 @@ public class battleSystem : MonoBehaviour
         setedPlayerTurn = false;
         setedEnemyTurn = false;
         state = BattleState.START;
-        enemyPrefab.GetComponent<TimerForTurn>().Reiniciar();
+        
     } 
 
     void CreateLisT()
@@ -230,7 +274,7 @@ public class battleSystem : MonoBehaviour
             }
             if (chars[i].GetComponent<Unit>().unitName == "Enemy" && enemyHasPlayed == false)
             {
-                state =BattleState.ENEMYTURN;
+                state = BattleState.ENEMYTURN;
                 break;
             }
             if (chars[i].GetComponent<Unit>().unitName == "EOR")
@@ -248,25 +292,28 @@ public class battleSystem : MonoBehaviour
             case 0:
                 if(chars[0].GetComponent<Unit>().unitName == "Player")
                 {
-                    print("TAKE THE MEDICINE");
                     skullHud.transform.position = pos1.transform.position;
+                    Comp1Hud.transform.position = pos3.transform.position;
                 }
                 else if(chars[0].GetComponent<Unit>().unitName == "Enemy")
                 {
                     zeroHud.transform.position = pos1.transform.position;
+                    Comp2Hud.transform.position = pos3.transform.position;
                 }
-                print("case 1");
+                
                 break;
             case 1:
                 if(chars[1].GetComponent<Unit>().unitName == "Player")
                 {
                     skullHud.transform.position = pos2.transform.position;
+                    Comp1Hud.transform.position = pos4.transform.position;
                 }
                 else if (chars[1].GetComponent<Unit>().unitName == "Enemy")
                 {
                     zeroHud.transform.position = pos2.transform.position;
+                    Comp2Hud.transform.position = pos4.transform.position;
                 }
-                print("case 2");
+                
                 break;
 
             case 3:
---
+++ Assets/Scripts/c_action.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c_action : MonoBehaviour
+{
+    public GameObject HudPanel;
+    public GameObject OutSqr;
+    [SerializeField] private float inSpeed;
+    public Transform pos_inSqr;
+    private bool walking;
+    bool acertou;
+
+    bool returnErrou;
+    bool returnAcertou;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(walking)
+        {
+           transform.position = new Vector2(transform.position.x + inSpeed * Time.deltaTime, transform.position.y);
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if (acertou)
+            {
+                returnAcertou = acertou;
+            }
+            else
+            {
+                returnErrou = true;
+                returnAcertou = false;
+            }
+                
+            
+            walking = false;
+        }
+
+    }
+
+    public void Resetar()
+    {
+        transform.position = pos_inSqr.transform.position;
+        HudPanel.gameObject.SetActive(false);
+        returnAcertou = false;
+        returnErrou = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+        if(collision.gameObject.name =="outSqr")
+        {
+         
+            acertou = true;
+        }
+        if(collision.gameObject.name == "inSqrFinalPosition")
+        {
+            walking = false;
+
+            returnErrou = true;
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+      
+        acertou = false;
+    }
+
+    public void Ativar()
+    {
+        HudPanel.gameObject.SetActive(true);
+        StartCoroutine(walkingAtivar());
+    }
+    IEnumerator walkingAtivar()
+    {
+        yield return new WaitForSeconds(1);
+        walking = true;
+    }
+    public bool Retornar()
+    {
+        return returnAcertou;
+    }
+    public bool RetornarErro()
+    {
+        return returnErrou;
+    }
+}
