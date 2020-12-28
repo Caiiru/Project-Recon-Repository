2f2a70fc3b099dcbcc220a5a8b0280391d19d5dc
Caiiru <caiogome125@gmail.com>
2020-12-28 14:58:06 -0300

    visualranger

--- Assets/Scripts/playerScripts/Skills/Sets/Scout/VaultStrike.cs
+++ Assets/Scripts/playerScripts/Skills/Sets/Scout/VaultStrike.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class VaultStrike:Skill
 {
+<<<<<<< Updated upstream
     public VaultStrike(){
 
 
     }
+=======
+    public VaultStrike()
+    {
+
+
+    }
+
+    private GameObject baixo;
+    private GameObject cima;
+    private GameObject esquerda;
+    private GameObject direita;
+
+    private Animator animbaixo;
+    private Animator animcima;
+    private Animator animesquerda;
+    private Animator animdireita;
+
+    [SerializeField] LayerMask layermask;
+
+    [SerializeField] bool usingSkill = false;
+    
+>>>>>>> Stashed changes
 
 
     public void Attack(){
